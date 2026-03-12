34e1d7537f84371d9ad88c9f9d915b6156459340
finlayrossdavie <finlayrossdavie@gmail.com>
2026-03-12 14:30:46 +0000

    useful functions

---
+++ utils.py
@@ -0,0 +1,130 @@
+def parse_lexicon(lex_file):
+    """
+    Parse the lexicon file and return it in dictionary form.
+    
+    Args:
+        lex_file (str): filename of lexicon file with structure '<word> <phone1> <phone2>...'
+                        eg. peppers p eh p er z
+
+    Returns:
+        lex (dict): dictionary mapping words to list of phones
+    """
+    
+    lex = {}  # create a dictionary for the lexicon entries (this could be a problem with larger lexica)
+    with open(lex_file, 'r') as f:
+        for line in f:
+            line = line.split()  # split at each space
+            lex[line[0]] = line[1:]  # first field the word, the rest is the phones
+    return lex
+
+def generate_symbol_tables(lexicon, n=3):
+    '''
+    Return word, phone and state symbol tables based on the supplied lexicon
+        
+    Args:
+        lexicon (dict): lexicon to use, created from the parse_lexicon() function
+        n (int): number of states for each phone HMM
+        
+    Returns:
+        word_table (fst.SymbolTable): table of words
+        phone_table (fst.SymbolTable): table of phones
+        state_table (fst.SymbolTable): table of HMM phone-state IDs
+    '''
+    
+    state_table = fst.SymbolTable()
+    phone_table = fst.SymbolTable()
+    word_table = fst.SymbolTable()
+    
+    # add empty <eps> symbol to all tables
+    state_table.add_symbol('<eps>')
+    phone_table.add_symbol('<eps>')
+    word_table.add_symbol('<eps>')
+    
+    for word, phones  in lexicon.items():
+        
+        word_table.add_symbol(word)
+        
+        for p in phones: # for each phone
+            
+            phone_table.add_symbol(p)
+            for i in range(1,n+1): # for each state 1 to n
+                state_table.add_symbol('{}_{}'.format(p, i))
+            
+    return word_table, phone_table, state_table
+
+
+# call these two functions
+lex = parse_lexicon('lexicon.txt')
+word_table, phone_table, state_table = generate_symbol_tables(lex)
+
+def generate_phone_wfst(f, start_state, phone, n):
+    """
+    Generate a WFST representating an n-state left-to-right phone HMM
+    
+    Args:
+        f (fst.Fst()): an FST object, assumed to exist already
+        start_state (int): the index of the first state, assmed to exist already
+        phone (str): the phone label 
+        n (int): number of states for each phone HMM
+        
+    Returns:
+        the final state of the FST
+    """
+    
+    current_state = start_state
+    
+    for i in range(1, n+1):
+        
+        in_label = state_table.find('{}_{}'.format(phone, i))
+        
+        sl_weight = fst.Weight('log', -math.log(0.1))  # weight for self-loop
+        # self-loop back to current state
+        f.add_arc(current_state, fst.Arc(in_label, 0, sl_weight, current_state))
+        
+        # transition to next state
+        
+        # we want to output the phone label on the final state
+        # note: if outputting words instead this code should be modified
+        if i == n:
+            out_label = phone_table.find(phone)
+        else:
+            out_label = 0   # output empty <eps> label
+            
+        next_state = f.add_state()
+        next_weight = fst.Weight('log', -math.log(0.9)) # weight to next state
+        f.add_arc(current_state, fst.Arc(in_label, out_label, next_weight, next_state))    
+       
+        current_state = next_state
+        
+    return current_state
+
+def generate_word_wfst(word):
+    """ Generate a WFST for any word in the lexicon, composed of 3-state phone WFSTs.
+        This will currently output word labels.  
+        Exercise: could you modify this function and the one above to output a single phone label instead?
+    
+    Args:
+        word (str): the word to generate
+        
+    Returns:
+        the constructed WFST
+    
+    """
+    f = fst.Fst('log')
+    
+    # create the start state
+    start_state = f.add_state()
+    f.set_start(start_state)
+    
+    current_state = start_state
+    
+    # iterate over all the phones in the word
+    for phone in lex[word]:   # will raise an exception if word is not in the lexicon
+        
+        current_state = generate_phone_wfst(f, current_state, phone, 3)
+    
+        # note: new current_state is now set to the final state of the previous phone WFST
+        
+    f.set_final(current_state)
+    
+    return f
